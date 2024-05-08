a36e32c5135d6b3a4458702c9bb1640af08e52e3
MarcoMunozOvalle2019e <marco.munoz.ovalle2010@gmaile.com>
2024-05-08 11:34:52 -0400

    feat: se crean los metodos del crud en typescript

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,9 +1,11 @@
 import  express from "express"
 import cors from "cors"
+import usuariosRouter from "./modules/usersRouter"
 export const app=express()
 
 app.use(cors()) //politicas de seguridad
 app.use(express.json())
+app.use("/api",usuariosRouter)
 
 const PORT=process.env.PORT || 3000
 export const server=app.listen(PORT,()=>{
---
+++ back-end/modules/usersRouter.ts
@@ -0,0 +1,24 @@
+import * as express from "express"
+const usuariosRouter = express.Router();
+import { Request,Response } from "express";
+
+
+usuariosRouter.get("/csv",async(req:Request,res:Response)=>{    
+})
+
+usuariosRouter.get("/",(req:Request,res:Response)=>{
+})
+
+usuariosRouter.get("/user",(req:Request,res:Response)=>{
+})
+
+usuariosRouter.post("/",(req:Request,res:Response)=>{
+})
+
+usuariosRouter.put("/:id",(req,res)=>{
+})
+
+usuariosRouter.delete("/:id",(req:Request,res:Response)=>{
+})
+
+export default usuariosRouter
