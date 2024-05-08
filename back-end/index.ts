243376bd3e7d9ed50a3d999af9a50bbe355b1ab2
MarcoMunozOvalle2019e <marco.munoz.ovalle2010@gmaile.com>
2024-05-08 11:29:53 -0400

    feat: se levanta un servidor express port 3000t

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,1 +1,12 @@
-console.log('inicio')
+import  express from "express"
+import cors from "cors"
+export const app=express()
+
+app.use(cors()) //politicas de seguridad
+app.use(express.json())
+
+const PORT=process.env.PORT || 3000
+export const server=app.listen(PORT,()=>{
+    console.log("servidos escuchando en puerto 3000")
+})
+server.on("error",console.error)
